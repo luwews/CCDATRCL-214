04d3a68fc32d629857c4e2f42dfe4d9bcd7cbd8e
luwews <102783085+luwews@users.noreply.github.com>
2022-08-23 14:43:00 +0800

    Create noconstructor.java

---
+++ withand without/noconstructor.java
@@ -0,0 +1,21 @@
+public class noconstructor {
+    
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+    public void hiMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    public void hiMyStrength() {
+        System.out.println("my strength is " + strength);
+    }
+
+    public void hiMyAgility() {
+        System.out.println("my agi is " + agility);
+    }
+
+    public void hiMyintellegence() {
+        System.out.println("my int is " + intelligence);
+    }
+}
