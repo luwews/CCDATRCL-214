0d590a944635e9067cd8862be0efa7c7e2f9a857
luwews <102783085+luwews@users.noreply.github.com>
2022-08-26 14:01:10 +0800

    Create Weapon.java

---
+++ Activity_1/Weapon.java
@@ -0,0 +1,13 @@
+public class Weapon {
+    String Name;
+    int Damage;
+    double Weight;
+    String Rarity;
+    String Description;
+
+public void WeaponDescription() {
+System.out.println(Name+"  Weapon Description: "+ Description);
+
+
+}    
+}
