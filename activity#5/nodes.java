1ae7b366bed4f164dd969bd283dcfff32a4cbabf
luwews <102783085+luwews@users.noreply.github.com>
2022-09-27 14:51:13 +0800

    Create nodes.java

---
+++ activity#5/nodes.java
@@ -0,0 +1,25 @@
+public class nodes {
+    
+    
+    String data;
+
+    nodes left;
+
+    
+    nodes right;
+
+    nodes middle;
+
+    
+    public nodes(String data) {
+
+       
+        this.data = data;
+
+       
+        left = null;
+        right = null;
+        middle=null;
+
+}
+}
